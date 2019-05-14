0774e5f8b1ab7cef4c4dd714b68a18439d28dfc1
sairim <spiri@protonmail.com>
2019-05-14 17:41:25 +0300

    de-"classify" DLL

--- section20-doubly-linked-lists/doublyLinkedList.js
+++ section20-doubly-linked-lists/doublyLinkedList.js
@@ -1,20 +1,10 @@
-class Node {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
-        this.prev = null;
-    }
-}
-
-class DoublyLinkedList {
-    constructor() {
-        this.head = null;
-        this.tail = null;
-        this.length = 0;
-    }
+var doublyLinkedList = {
+    head: null,
+    tailL: null,
+    length: 0,
 
     push(value) {
-        const newNode = new Node(value);
+        var newNode = node(value);
 
         if (!this.head) {
             this.head = newNode;
@@ -27,15 +17,15 @@ class DoublyLinkedList {
 
         this.length++;
         return this;
-    }
+    },
 
     pop() {
         if (!this.head) {
             return null;
         }
 
-        const oldTail = this.tail;
-        if (this.lenght === 1) {
+        var oldTail = this.tail;
+        if (this.length == 1) {
             this.head = null;
             this.tail = null;
         } else {
@@ -46,15 +36,15 @@ class DoublyLinkedList {
 
         this.length--;
         return oldTail;
-    }
+    },
 
     shift() {
         if (!this.head) {
             return null;
         }
 
-        const oldHead = this.head;
-        if (this.length === 1) {
+        var oldHead = this.head;
+        if (this.length == 1) {
             this.head = null;
             this.tail = null;
         } else {
@@ -65,10 +55,10 @@ class DoublyLinkedList {
 
         this.length--;
         return oldHead;
-    }
+    },
 
     unshift(value) {
-        const newNode = new Node(value);
+        var newNode = node(value);
 
         if (!this.head) {
             this.head = newNode;
@@ -81,15 +71,15 @@ class DoublyLinkedList {
 
         this.length++;
         return this;
-    }
+    },
 
     get(index) {
         if (index < 0 || index >= this.length) {
             return null;
         }
 
-        let foundNode;
-        const middle = Math.floor(this.length / 2);
+        var foundNode;
+        var middle = Math.floor(this.length / 2);
 
         if (index <= middle) {
             foundNode = this.head;
@@ -103,30 +93,30 @@ class DoublyLinkedList {
             }
         }
         return foundNode;
-    }
+    },
 
     set(index, value) {
-        const foundNode = this.get(index);
+        var foundNode = this.get(index);
         if (foundNode) {
             foundNode.value = value;
             return true;
         }
         return false;
-    }
+    },
 
     insert(index, value) {
         if (index < 0 || index >= this.length) {
             return false;
         }
-        if (index === 0) {
+        if (index == 0) {
             return this.unshift(value);
         }
-        if (index === this.length - 1) {
+        if (index == this.length - 1) {
             return this.push(value);
         }
 
-        const newNode = new Node(value);
-        const prevNode = this.get(index - 1);
+        var newNode = node(value);
+        var prevNode = this.get(index - 1);
     
         newNode.prev = prevNode;
         newNode.next = prevNode.next;
@@ -135,20 +125,20 @@ class DoublyLinkedList {
 
         this.length++;
         return true;
-    }
+    },
 
     remove(index) {
         if (index < 0 || index >= this.length) {
             return undefined;
         }
-        if (index === 0) {
+        if (index == 0) {
             return this.shift();
         }
-        if (index === this.length - 1) {
+        if (index == this.length - 1) {
             return this.pop();
         }
 
-        const removedNode = this.get(index);
+        var removedNode = this.get(index);
         removedNode.prev.next = removedNode.next;
         removedNode.next.prev = removedNode.prev;
         removedNode.prev = null;
@@ -159,7 +149,15 @@ class DoublyLinkedList {
     }
 }
 
-list = new DoublyLinkedList();
+function node(value) {
+    return {
+        value,
+        next: null,
+        prev: null
+    };
+}
+
+list = Object.create(doublyLinkedList);
 
 list.push(100);
 list.push(82);
