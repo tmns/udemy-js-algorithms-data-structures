cf87fc50029c048011bb02620f0726ebb7ce90bd
sairim <spiri@protonmail.com>
2019-05-14 18:00:22 +0300

    de-"classify" BST

--- section22-binary-search-trees/binarySearchTree.js
+++ section22-binary-search-trees/binarySearchTree.js
@@ -1,73 +1,71 @@
-class Node {
-    constructor(value) {
-        this.value = value;
-        this.left = null;
-        this.right = null;
+var binarySearchTree = {
+  root: null,
+
+  insert(value) {
+    var newNode = node(value);
+
+    if (!this.root) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (value == currentNode.value) {
+          return undefined;
+        }
+        if (value < currentNode.value) {
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            break;
+          }
+          currentNode = currentNode.left;
+        }
+        if (value > currentNode.value) {
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            break;
+          }
+          currentNode = currentNode.right;
+        }
+      }
     }
+    return this;
+  },
+
+  find(value) {
+    if (!this.root) {
+      return undefined;
+    }
+
+    var currentNode = this.root;
+    while (true) {
+      if (value == currentNode.value) {
+        return currentNode;
+      }
+      if (value < currentNode.value) {
+        if (!currentNode.left) {
+          return undefined;
+        }
+        currentNode = currentNode.left;
+      }
+      if (value > currentNode.value) {
+        if (!currentNode.right) {
+          return undefined;
+        }
+        currentNode = currentNode.right;
+      }
+    }
+  }
+};
+
+function node(value) {
+  return {
+    value,
+    left: null,
+    right: null
+  };
 }
 
-class BinarySearchTree {
-    constructor() {
-        this.root = null;
-    }
-
-    insert(value) {
-        const newNode = new Node(value);
-
-        if (!this.root) {
-            this.root = newNode;
-        } else {
-            let currentNode = this.root;
-            while (true) {
-                if (value === currentNode.value) {
-                    return undefined;
-                }
-                if (value < currentNode.value) {
-                    if (!currentNode.left) {
-                        currentNode.left = newNode;
-                        break;
-                    }
-                    currentNode = currentNode.left;
-                }
-                if (value > currentNode.value) {
-                    if (!currentNode.right) {
-                        currentNode.right = newNode;
-                        break;
-                    }
-                    currentNode = currentNode.right;
-                }
-            }
-        }
-        return this;
-    }
-
-    find(value) {
-        if (!this.root) {
-            return undefined;
-        }
-
-        let currentNode = this.root;
-        while (true) {
-            if (value === currentNode.value) {
-                return currentNode;
-            }
-            if (value < currentNode.value) {
-                if (!currentNode.left) {
-                    return undefined;
-                }
-                currentNode = currentNode.left;
-            }
-            if (value > currentNode.value) {
-                if (!currentNode.right) {
-                    return undefined;
-                }
-                currentNode = currentNode.right;
-            }
-        }
-    }
-}
-
-const bst = new BinarySearchTree();
+const bst = Object.create(binarySearchTree);
 
 bst.insert(10);
 bst.insert(1);
@@ -75,7 +73,7 @@ bst.insert(44);
 bst.insert(33);
 bst.insert(1);
 
-console.log (bst);
+console.log(bst);
 
 console.log(bst.find(44));
 console.log(bst.find(12));
